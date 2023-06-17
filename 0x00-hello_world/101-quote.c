4086e44ef48c5e010317e0da2806f478ae2e3578
Prosper <mesos4prosper@yahoo.com>
2023-06-17 14:49:47 -0700

    No printf or puts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
+#include <string.h>
 /**
- * 
- *
- */
+*main - entry poin
+*Return: Always 1 to standard error
+*/
+int main(void)
+{
+	char str[] =
+		"\"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	fwrite(str, sizeof(char), strlen(str), stderr);
+	return (1);
+}
