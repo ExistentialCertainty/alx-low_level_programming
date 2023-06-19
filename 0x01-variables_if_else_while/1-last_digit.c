838d0fe21bc043853ee6c106eebfac78b6315a97
Prosper <mesos4prosper@yahoo.com>
2023-06-19 09:05:02 -0700

    Last digit updated

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,16 +14,15 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	p = n % 10;
-	if (p > 5)
-	{
-		printf("The Last digit of %d is %d and is greater than 5\n", n, p);
-	} else if (p == 0)
-	{
-		printf("The Last digit of %d is %d and is 0\n", n, p);
-	} else
+	if (p < 6 && p != 0)
 	{
 		printf("The Last digit of %d is %d and is less 6 than and not 0\n", n, p);
+	} else if (p > 5)
+	{
+		printf("The Last digit of %d is %d and is greater than 5\n", n, p);
+	} else
+	{
+		printf("The Last digit of %d is %d and is 0\n", n, p);
 	}
 	return (0);
 }
-printf("The Last digit of %d is %d and is less 6 than and not 0\n", n, p);
