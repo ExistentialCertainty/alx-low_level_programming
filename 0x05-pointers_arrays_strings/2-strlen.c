df1d74ec6a04393fc68aa9882b482a0e74b32e00
Prosper <mesos4prosper@yahoo.com>
2023-07-07 11:13:56 -0700

    String length quantifier

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+*_strlen -  a function that returns
+*the length of a string.
+*
+*@s: pointer to string whose
+*length is to be determined
+*Return: count value
+*/
+int _strlen(char *s)
+{
+	int count;
+	int i;
+
+	count = 0;
+	i = 0;
+	while (s[i] != '\0')
+	{
+		count++;
+		i++;
+	}
+	return (count);
+}
