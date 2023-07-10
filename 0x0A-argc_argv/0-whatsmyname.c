4560bfe955956c3b01bd5e9d28f1eadebb53e419
Prosper <mesos4prosper@yahoo.com>
2023-07-09 18:42:13 -0700

    Identity debugging

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,13 +6,13 @@
  * @argv: string argument
  * Return: Success
  */
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int i;
 
 	for (i = 0; i < argc; i++)
 	{
-		printf("%s \n", argv[0]);
+		printf("%s\n", argv[0]);
 	}
 	return (0);
 }
