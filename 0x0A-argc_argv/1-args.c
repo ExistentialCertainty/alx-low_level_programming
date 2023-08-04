83f76582574478247a0a87318dc38b014e8a4783
ExistentialCertainty <mesos4prosper@yahoo.com>
2023-08-04 05:51:05 -0700

    argument count

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -  a program that prints the number of
+ * arguments passed into it.
+ *
+ * @argc: argument count
+ * @argv: argument vector containing an array of values
+ * Return: 0 success
+ */
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	printf("%d \n", argc - 1);
+	return (0);
+}
