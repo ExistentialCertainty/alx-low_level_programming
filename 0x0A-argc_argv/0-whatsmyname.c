e731970116e2c62578c84d2d46b5e819e59514ff
ExistentialCertainty <mesos4prosper@yahoo.com>
2023-08-04 05:27:56 -0700

    file identity

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -  a program that prints its name, followed
+ * by a new line. If you rename the program, it will
+ * print the new name, without having to compile it again
+ *
+ * @argc: argument count
+ * @argv: argument vector containing an array of values
+ * Return: 0 success
+ */
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s \n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,2 @@
+Month 2: ALXSE Programme
+Project containing tasks on argc and argv
