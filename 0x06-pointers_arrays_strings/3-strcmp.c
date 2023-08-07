184b416e88d520df0be7e5ca0005e6ff9074ed95
ExistentialCertainty <mesos4prosper@yahoo.com>
2023-08-07 05:39:36 -0700

    string compare

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,34 +3,20 @@
 /**
  * _strcmp -  a function that compares two strings.
  *
- * s1: first string argument to be compared with second.
- * s2: second string argument to be compared with first.
- * Return: -15 if s1 > s2; 15 if s2 > s1; 0 if s1 = s2.
+ * @s1: first string argument to be compared with second.
+ * @s2: second string argument to be compared with first.
+ * Return: 0 if s1 = s2.
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i, j;
-
-	i = 0;
-	j = 0;
-	while (s1[i] != '\0')
+	while (*s1 != '\0' || *s2 != '\0')
 	{
-		i++;
+		if (*s1 != *s2)
+		{
+			return (*s1 - *s2);
+		}
+		s1++;
+		s2++;
 	}
-	while (s2[j] != '\0')
-	{
-		j++;
-	}
-	if (i < j)
-	{
-		return (-15);
-	}
-	else if (i > j)
-	{
-		return (15);
-	}
-	else
-	{
-		return(0);
-	}
+	return (0);
 }
