e9de26d19de6aec108e91a8ead8d90af454483d1
ExistentialCertainty <mesos4prosper@yahoo.com>
2023-08-06 21:16:51 -0700

    string compare

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * _strchr - a function that locates a character in a string.
+ * @s: string argument to be compared against
+ * @c: character argument to be compared to s
+ *
+ * Return: pointer (s) to the first occurrence of c in s;
+ * NULL if the character is not found.
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	if (c == '\0')
+	{
+		return (s);
+	}
+	return ('\0');
+}
---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * _strstr - a function finds the first occurrence of the
+ * substring needle in the string haystack. The terminating
+ * null bytes (\0) are not compared
+ * @haystack: first string argument
+ * @needle: second string argument
+ *
+ * Return:  pointer to the beginning of the located substring,
+ * or NULL if the substring is not found.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	char *h;
+	char *n;
+
+	if (*needle == '\0')
+	{
+		return (haystack);
+	}
+	while (*haystack != '\0')
+	{
+		h = haystack;
+		n = needle;
+		while (*n != '\0' && *h == *n)
+		{
+			h++;
+			n++;
+		}
+		if (*n == '\0')
+		{
+			return (haystack);
+		}
+		haystack++;
+	}
+	return ('\0');
+}
