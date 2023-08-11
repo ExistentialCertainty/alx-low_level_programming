fdae1852658ed8f5f88a0c81927fcb1e522a760e
ExistentialCertainty <mesos4prosper@yahoo.com>
2023-08-11 04:22:15 -0700

    Str concat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -42,7 +42,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		i++;
 	}
 	j = 0;
-	while (s2[j] != '\0' && j <= n)
+	while (s2[j] != '\0' && j <= n - 1)
 	{
 		buffer[i] = s2[j];
 		i++;
