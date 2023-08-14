52ed452158ae9d0f05c803d48d4f933431724c44
ExistentialCertainty <mesos4prosper@yahoo.com>
2023-08-14 16:21:38 -0700

    Define Sum

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,3 +1,8 @@
+#ifndef HEADER_F
+#define HEADER_F
+
 #ifndef ABS
 #define ABS(x) ((x) >= 0 ? (x) : -(x))
+#endif
+
 #endif
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,8 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#ifndef SUM
+#define SUM(x, y) ((x) + (y))
+#endif
+
+#endif
