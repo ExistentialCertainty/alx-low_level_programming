70be14ee51d33b1251f3952f089163fd3a37df72
ExistentialCertainty <mesos4prosper@yahoo.com>
2023-08-14 10:08:46 -0700

    Poppy1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,16 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
+/**
+ * struct dog - function that contains the attributes of a dog.
+ * @name: Name argument of the dog.
+ * @age: Age argument of the dog.
+ * @owner: Owner argument of the dog.
+ */
 struct dog
 {
-        char *name;
-        float age;
-        char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
 
 #endif
