2d3c1305f26a0b0dd1b81e6ed698935e67364c10
ExistentialCertainty <mesos4prosper@yahoo.com>
2023-08-16 15:52:24 -0700

    Print Name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * print_name - A function that prints name
+ * @name: argument to be printed
+ * @f: function pointer to name
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
