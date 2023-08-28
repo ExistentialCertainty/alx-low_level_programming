978916424b3d5a851e44ee5c01c61774de45478d
ExistentialCertainty <mesos4prosper@yahoo.com>
2023-08-28 07:01:10 -0700

    print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -5,7 +5,7 @@
  * print_list - A function that prints all the elements of a
  * single linked list
  * @h: pointer to head of single linked lists
- * Return: printed list
+ * Return: printed list and node number
  */
 size_t print_list(const list_t *h)
 {
@@ -13,7 +13,7 @@ size_t print_list(const list_t *h)
 
 	while (h)
 	{
-		if (h->str == NULL)
+		if (!h->str)
 		{
 			printf("[0] (nil)\n");
 		}
