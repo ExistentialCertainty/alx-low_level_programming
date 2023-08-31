a74286abf946c50b92312bd97021906e11838fe8
ExistentialCertainty <mesos4prosper@yahoo.com>
2023-08-30 21:41:47 -0700

    upp

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * sum_ listint -  a function that returns the
+ * sum_listint -  a function that returns the
  * sum of all the data (n) of a listint_t linked list.
  * @head: pointer to head of listint_t list.
  * Return: number of elements in listint_t list.
