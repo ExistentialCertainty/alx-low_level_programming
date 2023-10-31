bcf7070f5e80a899d0b18fa63df92b308cbdd3aa
ExistentialCertainty <mesos4prosper@yahoo.com>
2023-10-31 07:08:43 -0700

    1-c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,6 +15,10 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
+	if (text_content == NULL)
+	{
+		text_content = "";
+	}
 	if (text_content != NULL)
 	{
 		for (len = 0; text_content[len];)
